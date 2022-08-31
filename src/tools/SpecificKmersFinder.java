2606b3a1522b21e779b60822b0b1547e7a4a5743
Владимир <52621625+vulden@users.noreply.github.com>
2022-08-31 12:56:25 +0300

    Add bonferroni correction and fix minor errors

--- src/tools/SpecificKmersFinder.java
+++ src/tools/SpecificKmersFinder.java
@@ -12,7 +12,6 @@ import ru.ifmo.genetics.utils.Misc;
 import ru.ifmo.genetics.utils.tool.ExecutionFailedException;
 import ru.ifmo.genetics.utils.tool.Parameter;
 import ru.ifmo.genetics.utils.tool.Tool;
-import ru.ifmo.genetics.utils.tool.inputParameterBuilder.DoubleParameterBuilder;
 import ru.ifmo.genetics.utils.tool.inputParameterBuilder.FileMVParameterBuilder;
 import ru.ifmo.genetics.utils.tool.inputParameterBuilder.FileParameterBuilder;
 
@@ -42,13 +41,11 @@ public class SpecificKmersFinder extends Tool {
     public final Parameter<Double> PValueChi2 = addParameter(new DoubleParameterBuilder("p-value-chi2")
             .withShortOpt("pchi2")
             .withDescription("p-value for chi-squared test")
-            .withDefaultValue(0.05)
             .create());
 
     public final Parameter<Double> PValueMW = addParameter(new DoubleParameterBuilder("p-value-mw")
             .withShortOpt("pmw")
             .withDescription("p-value for Mann-Whitney test")
-            .withDefaultValue(0.05)
             .create());
 
     public final Parameter<File> outputDir = addParameter(new FileParameterBuilder("output-dir")
@@ -80,17 +77,18 @@ public class SpecificKmersFinder extends Tool {
         ChiSquaredDistribution xi = new ChiSquaredDistributionImpl(1, 0.0001);
         double qvalue;
         try {
-             qvalue = xi.inverseCumulativeProbability(1 - PValueChi2.get());
+            qvalue = xi.inverseCumulativeProbability(1 - PValueChi2.get());
         } catch (MathException e) {
             throw new ExecutionFailedException("Error calculating chi-squared value!", e);
         }
 
-        long totalkmers = 0;
-        long uniq = 0;
-        long uniq_not_filtered_kmers = 0;
-        long totalskippedchikmers = 0;
-        long totalskippedMWkmers = 0;
-        long totalkmersleft = 0;
+        long total_kmers = 0;
+        long unique = 0;
+        long unique_left = 0;
+        long skip_chi2 = 0;
+        long skip_MW = 0;
+        long a_kmers = 0;
+        long b_kmers = 0;
 
         File outDir = outputDir.get();
         if (!outDir.exists()) {
@@ -113,7 +111,7 @@ public class SpecificKmersFinder extends Tool {
                 MutableLongShortEntry entry = it.next();
                 long key = entry.getKey();
                 if (entry.getValue() > 0) {
-                    totalkmers++;
+                    total_kmers++;
                     int num_files = 0;
                     int n_0_A = 0;
                     int n_0_B = 0;
@@ -149,8 +147,10 @@ public class SpecificKmersFinder extends Tool {
                     boolean isUniq = (n_1_A == 0) || (n_1_B == 0);
                     if (isUniq) {
                         chisq_bool = true;
-                        uniq++;
+                        unique++;
                     }
+                    if((n_0_A+n_0_B)==0)    //in all files
+                        chisq_bool = true;
 
                     if (chisq_bool) {
                         int pass = 0;
@@ -161,48 +161,51 @@ public class SpecificKmersFinder extends Tool {
                                 pass = 1;
                         }
                         if (pass == 1) {
-                            totalskippedMWkmers++;
+                            skip_MW++;
                         } else {
                             if (isUniq) {
-                                uniq_not_filtered_kmers++;
+                                unique_left++;
                             }
-                            totalkmersleft++;
-
                             double mean_A = mean(groupA);
                             double mean_B = mean(groupB);
                             if (mean_A > mean_B) {
+                                a_kmers++;
                                 streamA.writeLong(key);
                                 streamA.writeShort((int) mean_A);
                             } else {
+                                b_kmers++;
                                 streamB.writeLong(key);
                                 streamB.writeShort((int) mean_B);
                             }
                         }
                     } else {
-                        totalskippedchikmers++;
+                        skip_chi2++;
                     }
                 }
             }
             debug("Finished processing file #" + cur_file);
-            debug("Processed " + totalkmers + " k-mers");
+            debug("Processed " + total_kmers + " k-mers");
             cur_file++;
         }
 
         streamA.close();
         streamB.close();
-
+        long totalkmersleft = a_kmers+b_kmers;
         info("Group A k-mers printed to " + fileA.getPath());
-        info("Group B k-mers printed to " + fileA.getPath());
+        info("Group B k-mers printed to " + fileB.getPath());
+        info("Total specific k-mers in Group A = " + a_kmers);
+        info("Total specific k-mers in Group B = " + b_kmers);
 
-        debug("Total kmers count = " + totalkmers);
-        debug("Total unique kmers = " + uniq);
-        debug("Total skipped by chi-squared test = " + totalskippedchikmers);
-        debug("Total skipped by Mann-Whitney test = " + totalskippedMWkmers);
-        debug("Total unique left = " + uniq_not_filtered_kmers);
+        debug("Total unique kmers = " + unique);
+        debug("Total skipped by chi-squared test = " + skip_chi2);
+        debug("Total skipped by Mann-Whitney test = " + skip_MW);
+        debug("Total unique left = " + unique_left);
         debug("Total kmers left = " + totalkmersleft);
         debug("Parameters used : PValueChi = " + PValueChi2.get() + " that gives " + qvalue + " threshold" );
 
         debug("Specific-kmers has finished! Time = " + t);
+        debug("Processed " + total_kmers + " k-mers");
+
     }
 
     private boolean chisq(int c0, int c1, int p0, int p1, double value) {
@@ -213,12 +216,9 @@ public class SpecificKmersFinder extends Tool {
         float x2 = gr_1 / all * (p0 + c0);
         float x3 = gr_2 / all * (p1 + c1);
         float x4 = gr_2 / all * (p0 + c0);
-        if (x1 != 0.0 && x2 != 0.0 && x3 != 0.0 && x4 != 0.0) {
-            double kk = Math.pow(Math.abs(p1 - x1), 2) / x1 + Math.pow(Math.abs(p0 - x2), 2) / x2 + Math.pow(Math.abs(c1 - x3), 2) / x3 + Math.pow(Math.abs(c0 - x4), 2) / x4;
-            return value < kk;
+        double kk = Math.pow(Math.abs(p1 - x1) - 0.5, 2) / x1 + Math.pow(Math.abs(p0 - x2)- 0.5, 2) / x2 + Math.pow(Math.abs(c1 - x3)- 0.5, 2) / x3 + Math.pow(Math.abs(c0 - x4)- 0.5, 2) / x4;
+        return value < kk;
         }
-        return false;
-    }
 
     private double mean(double[] m) {
         double sum = 0;
